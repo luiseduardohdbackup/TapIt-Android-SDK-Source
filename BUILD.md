8e913e01ef16e39ddd29ecd5677319678babc855
Nick Penteado <npenteado@phunware.com>
2015-01-26 11:27:13 -0800

    fixed wrong version code string

---
+++ BUILD.md
@@ -0,0 +1,4 @@
+put a copy of google play services in the same directory as sdk repository(build process has relative paths pointing to g.p.s.)... e.g.
+/path/to/
+  /sdk-android-ads/
+  /google-play-services_lib/
--- src/sdk/src/com/tapit/adview/AdViewCore.java
+++ src/sdk/src/com/tapit/adview/AdViewCore.java
@@ -43,7 +43,7 @@ import android.widget.FrameLayout;
  * Viewer of advertising.
  */
 public abstract class AdViewCore extends WebView {
-    public static final String VERSION = "2.0.4";
+    public static final String VERSION = "2.1.0";
     public static final String TAG = "TapIt";
 
     private static final long AD_DEFAULT_RELOAD_PERIOD = 120000; // milliseconds
